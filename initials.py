125875f61022087a9663f9417d4dd43fa6cdf81e
mykeesey <2425403047@live.gctu.edu.gh>
2026-05-31 09:33:16 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Michael Seyram Adzrakor
+# STUDENT ID: 2425403047
+# Email: 2425403047@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Michael Seyram Adzrakor, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in small and large stylized format.
 
+    print()
+    print("...MSA")
+    print()
+
+    print("...MMMMMMMMMMMM***SSSSSSSSSSSS***AAAAAAAAAAAA***")
+    print("...MM        MM***SS          ***AA        AA***")
+    print("...MMM      MMM***SS          ***AA        AA***")
+    print("...MM MM  MM MM***SSSSSSSSSSSS***AAAAAAAAAAAA***")
+    print("...MM  MMMM  MM***          SS***AA        AA***")
+    print("...MM   MM   MM***          SS***AA        AA***")
+    print("...MM        MM***SSSSSSSSSSSS***AA        AA***")
+    print("...MM        MM***SSSSSSSSSSSS***AA        AA***")
+    print("...MM        MM***SSSSSSSSSSSS***AA        AA***")
+    print("...MM        MM***SSSSSSSSSSSS***AA        AA***")
+    print()
 
 
 main()
